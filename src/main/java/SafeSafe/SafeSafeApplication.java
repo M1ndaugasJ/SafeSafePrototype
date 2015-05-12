a6331cb1239e9cf6664c0ba71d9072d58cfae599
iamsoheavy <ciamusu1@gmail.com>
2015-05-12 22:28:34 +0300

    Initial commit

---
+++ src/main/java/SafeSafe/SafeSafeApplication.java
@@ -0,0 +1,13 @@
+package SafeSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeSafeApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeSafeApplication.class, args);
+    }
+}
